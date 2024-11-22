069d9d1715ce5200637587bbaa8dcae4f33b1db6
Wellington Zaneze <wellington.zaneze@outlook.com.br>
2024-11-22 11:51:53 -0300

    Add new project

---
+++ dotnet-core/dotnet-core/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_core
+{
+    public record Product
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public int Price { get; set; }
+    }
+}
